27dd33169618ec3bebbf78aa7fabf844b8b3c56b
Efekan Yılmazer <84992900+efeyilmazer@users.noreply.github.com>
2021-06-24 23:48:20 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const minifyJS = require("gulp-uglify");
 const concat = require("gulp-concat");
 const autoprefixer = require("gulp-autoprefixer");
 const del = require("del");
-const fileName="20-login-component";
+const fileName="21-login-component";
 
 //src/2-linkedin-redesign/**/
 gulp.task("browser-sync", () => {
