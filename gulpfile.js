a9d4429b3be0964d03d782006216d75fccfbc84f
Efekan Yılmazer <84992900+efeyilmazer@users.noreply.github.com>
2021-07-06 15:59:42 +0300

    price plan component example
    Monthly/Yearly price selector

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const minifyJS = require("gulp-uglify");
 const concat = require("gulp-concat");
 const autoprefixer = require("gulp-autoprefixer");
 const del = require("del");
-const fileName="4-support-component";
+const fileName="5-price-plan-component";
 
 //src/2-linkedin-redesign/**/
 gulp.task("browser-sync", () => {
