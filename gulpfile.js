575ddf97ec517acd3d5e8cc11ca250915adaef86
Efekan Yılmazer <84992900+efeyilmazer@users.noreply.github.com>
2021-07-07 21:27:18 +0300

    gulpfile updated

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const minifyJS = require("gulp-uglify");
 const concat = require("gulp-concat");
 const autoprefixer = require("gulp-autoprefixer");
 const del = require("del");
-const fileName="5-price-plan-component";
+const fileName="6-share-popup-component";
 
 //src/2-linkedin-redesign/**/
 gulp.task("browser-sync", () => {
