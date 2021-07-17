14ff173297ff6603b99e404c36a448cf41a011af
Efekan Yılmazer <84992900+efeyilmazer@users.noreply.github.com>
2021-07-18 01:18:06 +0300

    gulpfile updated

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const minifyJS = require("gulp-uglify");
 const concat = require("gulp-concat");
 const autoprefixer = require("gulp-autoprefixer");
 const del = require("del");
-const fileName = "plan&history-component";
+const fileName = "12-trial-credit-component";
 
 //src/2-linkedin-redesign/**/
 gulp.task("browser-sync", () => {
