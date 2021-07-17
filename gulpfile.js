215932c4baeaf57aae9b1878bae7f887348059e0
Efekan Yılmazer <84992900+efeyilmazer@users.noreply.github.com>
2021-07-17 15:15:55 +0300

    gulpfile updated

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const minifyJS = require("gulp-uglify");
 const concat = require("gulp-concat");
 const autoprefixer = require("gulp-autoprefixer");
 const del = require("del");
-const fileName="11-footer-component";
+const fileName = "plan&history-component";
 
 //src/2-linkedin-redesign/**/
 gulp.task("browser-sync", () => {
