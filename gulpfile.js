4d9d69340a105ff38df2dd17bd62bf084ceaa474
Efekan Yılmazer <84992900+efeyilmazer@users.noreply.github.com>
2021-07-26 02:47:04 +0300

    updated

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const minifyJS = require("gulp-uglify");
 const concat = require("gulp-concat");
 const autoprefixer = require("gulp-autoprefixer");
 const del = require("del");
-const fileName = "Settings&Notification-component";
+const fileName = "21-safari-browser";
 
 //src/2-linkedin-redesign/**/
 gulp.task("browser-sync", () => {
